eeb02873f88403d77417fbd7679445212c134d94
Jacob-MC <jacobarvizu583@gmail.com>
2022-09-05 15:13:10 -0700

    Added checks for the commands

--- src/fr/anxxitty/srp/Commands.java
+++ src/fr/anxxitty/srp/Commands.java
@@ -36,118 +36,135 @@ public class Commands implements CommandExecutor {
             Player player = (Player) sender;
 
             if (cmd.getName().equalsIgnoreCase("startrun")) {
-                //deletes worlds
-                worldManager.deleteWorld("spworld");
-                worldManager.deleteWorld("spnether");
-                worldManager.deleteWorld("spend");
-
-                player.sendMessage("§3Starting speedrun! §6/!\\ §4Please don't move during the generation, this may crash the server §6/!\\");
-
-                //Removes all the data about the player (potion effect, health, inventory, advancements...)
-                player.getInventory().clear();
-                player.updateInventory();
-                player.setHealth(20.0);
-                player.setFoodLevel(20);
-                for (PotionEffect effect : player.getActivePotionEffects()) {
-                    player.removePotionEffect(effect.getType());
-                }
-                player.setExp(0);
-                player.setLevel(0);
-                player.setSaturation(20);
-
-                Iterator<Advancement> iterator = Bukkit.getServer().advancementIterator();
-                while (iterator.hasNext())
-                {
-                    AdvancementProgress progress = player.getAdvancementProgress(iterator.next());
-                    for (String criteria : progress.getAwardedCriteria())
-                        progress.revokeCriteria(criteria);
-                }
-
-                //Regenerates the worlds
-                worldManager.addWorld("spworld", World.Environment.NORMAL, null, WorldType.NORMAL, true, null);
-                String seed = String.valueOf(worldManager.getMVWorld("spworld").getSeed());
-                worldManager.addWorld("spnether", World.Environment.NETHER, seed, WorldType.NORMAL, true, null);
-                worldManager.addWorld("spend", World.Environment.THE_END, seed, WorldType.NORMAL, true, null);
-
-                worldManager.getMVWorld("spnether").setRespawnToWorld("spworld");
-                worldManager.getMVWorld("spend").setRespawnToWorld("spworld");
-
-                //Teleports the player at a safe location
-                Location originalLocation = worldManager.getMVWorld("spworld").getSpawnLocation();
-                Location safeLocation = core.getSafeTTeleporter().getSafeLocation(originalLocation);
-                worldManager.getMVWorld("spworld").setSpawnLocation(safeLocation);
-
-                player.sendMessage("§3Generation finished!");
-                player.sendMessage("§3Teleporting...");
-
-                player.teleport(safeLocation);
-
-                player.sendMessage("§3Teleported!");
-
-                return true;
-            }
-
-            if (cmd.getName().equalsIgnoreCase("endrun")) {
-                player.sendTitle("§4Run Stopped.", "§4Deleting Worlds.",10, 140, 20);
-                //Deletes the worlds
-
-                Bukkit.getScheduler().runTaskLater(speedRunPlugin, () -> {
+                // Checks if speedrun world exists
+                if (worldManager.getMVWorlds().toString().contains("spworld")) {
+                    player.sendMessage("§cYou are already in a speedrun! Use the reset command to reset the world, or the endrun command to end the run.");
+                    return true;
+                } else {
+                    //deletes worlds
                     worldManager.deleteWorld("spworld");
                     worldManager.deleteWorld("spnether");
                     worldManager.deleteWorld("spend");
-                }, 100);
-                return true;
+
+                    player.sendMessage("§3Starting speedrun! §6/!\\ §4Please don't move during the generation, this may crash the server §6/!\\");
+
+                    //Removes all the data about the player (potion effect, health, inventory, advancements...)
+                    player.getInventory().clear();
+                    player.updateInventory();
+                    player.setHealth(20.0);
+                    player.setFoodLevel(20);
+                    for (PotionEffect effect : player.getActivePotionEffects()) {
+                        player.removePotionEffect(effect.getType());
+                    }
+                    player.setExp(0);
+                    player.setLevel(0);
+                    player.setSaturation(20);
+
+                    Iterator<Advancement> iterator = Bukkit.getServer().advancementIterator();
+                    while (iterator.hasNext())
+                    {
+                        AdvancementProgress progress = player.getAdvancementProgress(iterator.next());
+                        for (String criteria : progress.getAwardedCriteria())
+                            progress.revokeCriteria(criteria);
+                    }
+
+                    //Regenerates the worlds
+                    worldManager.addWorld("spworld", World.Environment.NORMAL, null, WorldType.NORMAL, true, null);
+                    String seed = String.valueOf(worldManager.getMVWorld("spworld").getSeed());
+                    worldManager.addWorld("spnether", World.Environment.NETHER, seed, WorldType.NORMAL, true, null);
+                    worldManager.addWorld("spend", World.Environment.THE_END, seed, WorldType.NORMAL, true, null);
+
+                    worldManager.getMVWorld("spnether").setRespawnToWorld("spworld");
+                    worldManager.getMVWorld("spend").setRespawnToWorld("spworld");
+
+                    //Teleports the player at a safe location
+                    Location originalLocation = worldManager.getMVWorld("spworld").getSpawnLocation();
+                    Location safeLocation = core.getSafeTTeleporter().getSafeLocation(originalLocation);
+                    worldManager.getMVWorld("spworld").setSpawnLocation(safeLocation);
+
+                    player.sendMessage("§3Generation finished!");
+                    player.sendMessage("§3Teleporting...");
+
+                    player.teleport(safeLocation);
+
+                    player.sendMessage("§3Teleported!");
+
+                    return true;
+                }
+            }
+
+            if (cmd.getName().equalsIgnoreCase("endrun")) {
+                //Checks if the speedrun world exists
+                if (worldManager.getMVWorlds().toString().contains("spworld")) {
+                    player.sendMessage("§3Ending speedrun!");
+                    //deletes worlds
+                    Bukkit.getScheduler().runTaskLater(speedRunPlugin, () -> {
+                        worldManager.deleteWorld("spworld");
+                        worldManager.deleteWorld("spnether");
+                        worldManager.deleteWorld("spend");
+                        player.sendMessage("§3Speedrun ended!");
+                    }, 100);
+                    return true;
+                } else {
+                    player.sendMessage("§cThere is no speedrun to end!");
+                    return true;
+                }
             }
 
             if (cmd.getName().equalsIgnoreCase("reset")) {
+                //Checks if the speedrun world exists
+                if (worldManager.getMVWorlds().toString().contains("spworld")) {
+                    player.sendMessage("§3Resetting speedrun! §6/!\\ §4Please don't move during the generation, this may crash the server §6/!\\");
 
-                player.sendMessage("§3Resetting speedrun! §6/!\\ §4Please don't move during the generation, this may crash the server §6/!\\");
+                    //Removes all the data about the player (potion effect, health, inventory, advancements...)
+                    player.getInventory().clear();
+                    player.updateInventory();
+                    player.setHealth(20.0);
+                    player.setFoodLevel(20);
+                    for (PotionEffect effect : player.getActivePotionEffects()) {
+                        player.removePotionEffect(effect.getType());
+                    }
+                    player.setExp(0);
+                    player.setLevel(0);
+                    player.setSaturation(20);
 
-                //Removes all the data about the player (potion effect, health, inventory, advancements...)
-                player.getInventory().clear();
-                player.updateInventory();
-                player.setHealth(20.0);
-                player.setFoodLevel(20);
-                for (PotionEffect effect : player.getActivePotionEffects()) {
-                    player.removePotionEffect(effect.getType());
+                    Iterator<Advancement> iterator = Bukkit.getServer().advancementIterator();
+                    while (iterator.hasNext())
+                    {
+                        AdvancementProgress progress = player.getAdvancementProgress(iterator.next());
+                        for (String criteria : progress.getAwardedCriteria())
+                            progress.revokeCriteria(criteria);
+                    }
+                    //Regenerates the worlds
+                    worldManager.deleteWorld("spworld");
+                    worldManager.deleteWorld("spnether");
+                    worldManager.deleteWorld("spend");
+
+                    worldManager.addWorld("spworld", World.Environment.NORMAL, null, WorldType.NORMAL, true, null);
+                    String seed = String.valueOf(worldManager.getMVWorld("spworld").getSeed());
+                    worldManager.addWorld("spnether", World.Environment.NETHER, seed, WorldType.NORMAL, true, null);
+                    worldManager.addWorld("spend", World.Environment.THE_END, seed, WorldType.NORMAL, true, null);
+
+                    worldManager.getMVWorld("spnether").setRespawnToWorld("spworld");
+                    worldManager.getMVWorld("spend").setRespawnToWorld("spworld");
+
+                    //Teleports the player at a safe location
+                    Location originalLocation = worldManager.getMVWorld("spworld").getSpawnLocation();
+                    Location safeLocation = core.getSafeTTeleporter().getSafeLocation(originalLocation);
+                    worldManager.getMVWorld("spworld").setSpawnLocation(safeLocation);
+
+                    player.sendMessage("§3Generation finished!");
+                    player.sendMessage("§3Teleporting...");
+
+                    player.teleport(safeLocation);
+
+                    player.sendMessage("§3Teleported!");
+
+                    return true;
+                } else {
+                    player.sendMessage("§cThere is no speedrun to reset!");
+                    return true;
                 }
-                player.setExp(0);
-                player.setLevel(0);
-                player.setSaturation(20);
-
-                Iterator<Advancement> iterator = Bukkit.getServer().advancementIterator();
-                while (iterator.hasNext())
-                {
-                    AdvancementProgress progress = player.getAdvancementProgress(iterator.next());
-                    for (String criteria : progress.getAwardedCriteria())
-                        progress.revokeCriteria(criteria);
-                }
-                //Regenerates the worlds
-                worldManager.deleteWorld("spworld");
-                worldManager.deleteWorld("spnether");
-                worldManager.deleteWorld("spend");
-
-                worldManager.addWorld("spworld", World.Environment.NORMAL, null, WorldType.NORMAL, true, null);
-                String seed = String.valueOf(worldManager.getMVWorld("spworld").getSeed());
-                worldManager.addWorld("spnether", World.Environment.NETHER, seed, WorldType.NORMAL, true, null);
-                worldManager.addWorld("spend", World.Environment.THE_END, seed, WorldType.NORMAL, true, null);
-
-                worldManager.getMVWorld("spnether").setRespawnToWorld("spworld");
-                worldManager.getMVWorld("spend").setRespawnToWorld("spworld");
-
-                //Teleports the player at a safe location
-                Location originalLocation = worldManager.getMVWorld("spworld").getSpawnLocation();
-                Location safeLocation = core.getSafeTTeleporter().getSafeLocation(originalLocation);
-                worldManager.getMVWorld("spworld").setSpawnLocation(safeLocation);
-
-                player.sendMessage("§3Generation finished!");
-                player.sendMessage("§3Teleporting...");
-
-                player.teleport(safeLocation);
-
-                player.sendMessage("§3Teleported!");
-
-                return true;
             }
         }
         else {
